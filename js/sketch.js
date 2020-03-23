dfcb1352d08bfda365f2fce90520eb6b44216139
LearningMonkey61 <b118061@iiit-bh.ac.in>
2020-03-23 20:27:33 +0530

    First Commit

---
+++ js/sketch.js
@@ -0,0 +1,94 @@
+let button;
+
+let cureTime = 250;
+let cure = false;
+
+let particles = [];
+let slider;
+let numP = 200;
+let sdfactor;
+let infected = 0.01;
+
+let numInfec = 0;
+let numHealt = 0;
+let frameCount;
+
+
+
+function setup() {
+
+    var canvasDiv = document.getElementById('sketch-holder');
+
+
+    slider = createSlider(0, 1, 0.5, 0.05);
+    slider.parent('slider');
+
+    button = createButton('Reload')
+    button.mousePressed(reload);
+    button.parent('slider');
+
+
+    bgcanvas = createCanvas(canvasDiv.offsetWidth, canvasDiv.offsetHeight);
+    bgcanvas.style(
+        "cursor: none; z-index:-1000 ; "
+    );
+    bgcanvas.parent('sketch-holder');
+
+
+    reload();
+
+}
+
+function draw() {
+
+    if (frameCount == cureTime) {
+        cure = true;
+    }
+    numInfec = 0;
+    numHealt = 0;
+    background(0);
+    textSize(50);
+    fill(0, 102, 153);
+    text(sdfactor, 10, 60);
+
+    // document.getElementById("sdfactor").innerHTML = slider.value();
+
+
+    particles.forEach(p => {
+        p.collide();
+        p.move();
+        p.show();
+        if (p.infected == true) {
+            numInfec++;
+        } else if (p.infected == false) {
+            numHealt++;
+        }
+
+        if (cure == true || p.infected == true) {
+            p.curet--;
+            if (p.curet == 0) {
+                p.infected = false
+            }
+        }
+
+    });
+
+    frameCount++;
+
+}
+
+function reload() {
+    frameCount = 0;
+    for (let i = 0; i < numP; i++) {
+        particles[i] = new Particle(
+            random(width),
+            random(height),
+            i,
+            particles,
+            Math.random() < sdfactor ? 0 : 1,
+            Math.random() < infected ? true : false
+
+        );
+    }
+    sdfactor = slider.value();
+}
